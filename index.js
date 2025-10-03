ed9b2738775c24b69b8b3036f65ebe5f5ec560a5
desny <trsadesny@gmail.com>
2025-10-03 10:46:06 +0530

    javascript basis

---
+++ index.js
@@ -0,0 +1,8 @@
+function myFunc()
+{
+    if(true)
+    {
+        let a=20;
+        console.log(a)
+    }
+}
---
+++ variables.js
@@ -0,0 +1,21 @@
+var a=10;
+var helo=100;
+{
+    let c=40;
+
+    {
+        console.log(a)
+        {
+            let b=20;
+            {
+                console.log(b)
+            }
+            console.log(b)
+        }
+        console.log(c)
+    }
+    console.log(c)
+}
+console.log(a)
+
+console.log(helo)
