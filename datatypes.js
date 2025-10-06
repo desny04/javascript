6abed3e4db805e8a21b8b6aac8a09dcef1f9c190
desny <trsadesny@gmail.com>
2025-10-06 11:00:14 +0530

    javascript datatypes

---
+++ datatypes.js
@@ -0,0 +1,60 @@
+var a=0;
+console.log(typeof(a))
+
+var b="Desny";
+console.log(typeof(b))
+
+var c=true;
+console.log(typeof(c))
+
+var d=false
+console.log(typeof(d))
+
+var x=null;
+console.log(x)
+
+var y;
+console.log(typeof(y))
+
+var z=33n;
+console.log(typeof(z))
+
+var h=33n;
+var i=44;
+let sum=h+BigInt(i);
+console.log(sum)
+
+var g=Symbol("Desny")
+console.log(typeof(g))
+
+var k=("hyy")
+var l=("hyy")
+console.log(k==l)
+
+var m=['desny',21,'python',456]
+console.log(m)
+
+var n=['desny',21,'python',456]
+console.log(n[2])
+
+
+var w={
+    name:"Desny",
+    student:true,
+    place:"Kochi",
+    age:21,
+    aadhar:2354649993
+}
+
+console.log(w)
+
+var v={
+    name:"Desny",
+    student:true,
+    place:"Kochi",
+    age:21,
+    aadhar:2354649993
+}
+
+console.log(v.aadhar)
+
