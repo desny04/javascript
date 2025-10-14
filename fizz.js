c718904a7e4e84ce1a163daf9fbd7ec647132a31
desny <trsadesny@gmail.com>
2025-10-14 11:08:01 +0530

    javascript fizz

--- fizz.js
+++ fizz.js
@@ -1,11 +1,26 @@
 for(i=1;i<=100;i++)
 {
-    if(i%3==0)
+    if(i%3==0 && i%5==0)
+    {
+        console.log("fizz bus");
+    }
+    else if(i%3==0)
     {
         console.log("fizz");
     }
-    else
+
+    else if(i%5==0)
     {
-        console.log(i);
+        console.log("bus")
+    }
+
+    else{
+        console.log(i)
     }
 }
+
+for(i=100; i>=0; i--)
+{
+    console.log(i);
+}
+
