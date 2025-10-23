89b2c7dac294b67d2431c6c120d2cbea8851814c
desny <trsadesny@gmail.com>
2025-10-23 11:24:13 +0530

    javascript array

---
+++ array.js
@@ -0,0 +1,30 @@
+let arr=[1,2,3,4,5]
+
+// arr.push(6)
+
+//arr.pop()
+
+// arr.shift()
+
+// arr.unshift(70)
+
+// arr.splice(2,2,45)
+
+// let str=arr.join("-")
+
+// console.log(str)
+
+// let brr=[7,8,9]
+// let newarr=arr.concat(brr)
+
+// let newarr=arr.slice(3,6)
+// console.log(newarr)
+
+
+console.log(arr.indexOf(4))
+
+console.log(arr.includes(6))
+
+let newarr=["desny","francis", "name"]
+let chain=newarr.join(" ").toUpperCase()
+console.log(chain)
