5910f8d1a78a8f169cae0ba0cb9796251489e93b
desny <trsadesny@gmail.com>
2025-10-28 11:31:01 +0530

    javascript dom

---
+++ domwork.js
@@ -0,0 +1,21 @@
+let button=document.getElementById("btn")
+
+button.addEventListener("click",(event)=>{
+    event.preventDefault();
+
+    let name=document.getElementById("name").value;
+    let age=document.getElementById("age").value;
+    let mark=document.getElementById("mark").value;
+    let std=document.getElementById("std").value;
+
+    // let arr=[name,age,mark,std]
+    // console.log(arr)
+
+    document.getElementById("table").innerHTML+=`<tr>
+        <td>${name}</td>
+        <td>${age}</td>
+        <td>${mark}</td>
+        <td>${std}</td>
+    </tr>`
+
+})
